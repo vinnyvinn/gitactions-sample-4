fd11d3f51d3c67ea3d3b29fd79b4736dfb3e46e9
vinnyvinn <kituyiv@gmail.com>
2023-05-13 16:22:02 +0300

    added output

--- actions/deploy-s3-javascript/main.js
+++ actions/deploy-s3-javascript/main.js
@@ -11,6 +11,9 @@ function run() {
   //Upload Files
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  const websiteUrl=`http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url',websiteUrl);
   core.notice("Hello from custom actions");
 }
 
