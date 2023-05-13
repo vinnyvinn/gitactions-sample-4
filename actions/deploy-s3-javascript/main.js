6a5bc327ae89de7c05f1c0e2897aec8ba25db69a
vinnyvinn <kituyiv@gmail.com>
2023-05-13 14:01:17 +0300

    added first custom javascript action

--- actions/deploy-s3-javascript/main.js
+++ actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
-import core from '@actions/core';
-import exec from '@actions/exec';
-import github from '@actions/github';
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 function run() {
   core.notice("Hello from custom actions");
